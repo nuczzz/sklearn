a0416b2d0524935ab19d64610314024512fdcb83
nuczzz <nuc_zzz@163.com>
2019-05-05 10:32:06 +0800

    update 1 blank line to 2 blank line between funtions

--- iris/iris_train_test_split.py
+++ iris/iris_train_test_split.py
@@ -1,10 +1,12 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
+
 def iris_train_test_split():
 	iris_info = load_iris()
 	x_train, x_test, y_train, y_test = train_test_split(iris_info.data, iris_info.target, test_size=0.2, random_state=22)
 	print("x_train: \n", x_train)
+
 
 if __name__ == "__main__":
 	iris_train_test_split()
