78c4e0feb6227c1e7ab8663447f05efc48dcd743
nuczzz <nuc_zzz@163.com>
2019-05-09 08:26:43 -0700

    update text count vectorizer annotation

--- text/text_count_vectorizer.py
+++ text/text_count_vectorizer.py
@@ -4,7 +4,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 def text_count_vectorizer():
 	text = ["life is short, I like python", "life is too long, I dilike python"]
 	
-	#new default transform instance(sparse instance)
+	#CountVectorizer(stop_word=[])
+	#create a new sparse transform instance
 	transform_default = CountVectorizer()
 	data_default = transform_default.fit_transform(text)
 	print(data_default)
