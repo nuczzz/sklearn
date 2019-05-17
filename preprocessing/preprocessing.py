577d271ba657c68c0941ac874318b37728dda1cc
nuczzz <nuc_zzz@163.com>
2019-05-17 08:29:18 -0700

    update preprocessing

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,10 +1,19 @@
 from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
 #1.standard
 #2.normalization: x'=(x-min)/(max-min), x"=x'*(mx-mi)+mi
 
 
 def preprocessing_demo():
-	return None
+	#get data from file
+	data = pd.read_csv("data.txt")
+	data = data.iloc[:, :3]
+	
+	#
+	transfer = MinMaxScaler()
+	data_new = transfer.fit_transform(data)
+	print(data_new)
 
 
 if __name__ == "__main__":
